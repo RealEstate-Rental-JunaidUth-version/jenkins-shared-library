b568f9c6227600af62d64da03d3978d06ba9e19c
JunaidUthman <junaiduthman123@gmail.com>
2026-03-01 23:33:57 +0000

    first commit

---
+++ vars/pythonPipeline.groovy
@@ -0,0 +1,29 @@
+def call(Map config = [:]) {
+    pipeline {
+        agent any
+        environment {
+            DOCKER_IMAGE = "${config.dockerUser}/${config.appName}:${env.BUILD_NUMBER}"
+        }
+        stages {
+            stage('Lint & Test') {
+                steps { 
+                    sh 'pip install flake8 pytest'
+                    sh 'flake8 .'
+                    sh 'pytest'
+                }
+            }
+            stage('Build & Push') {
+                when { branch 'main' }
+                steps {
+                    script {
+                        sh "docker build -t ${DOCKER_IMAGE} ."
+                        withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                            sh "echo $PASS | docker login -u $USER --password-stdin"
+                            sh "docker push ${DOCKER_IMAGE}"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ vars/springBootPipeline.groovy
@@ -0,0 +1,29 @@
+def call(Map config = [:]) {
+    pipeline {
+        agent any
+        tools {
+            maven 'maven-3.9' 
+            jdk 'jdk-17'      
+        }
+        environment {
+            DOCKER_IMAGE = "${config.dockerUser}/${config.appName}:${env.BUILD_NUMBER}"
+        }
+        stages {
+            stage('Test') {
+                steps { sh 'mvn clean test' }
+            }
+            stage('Build & Push') {
+                when { branch 'main' }
+                steps {
+                    script {
+                        sh "docker build -t ${DOCKER_IMAGE} ."
+                        withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                            sh "echo $PASS | docker login -u $USER --password-stdin"
+                            sh "docker push ${DOCKER_IMAGE}"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
