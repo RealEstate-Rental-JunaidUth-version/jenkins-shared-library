1b9ee4df81135332cfc56e502ca0a9b3a04e2cf8
JunaidUthman <junaiduthman123@gmail.com>
2026-03-05 00:09:31 +0000

    define the variable at the verry top

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -1,14 +1,12 @@
 def call(Map config = [:]) {
+    def dockerImage = "${config.dockerUser}/${config.appName}:${env.BUILD_NUMBER}"
+    
     pipeline {
         agent any
-        environment {
-            DOCKER_IMAGE = "${config.dockerUser}/${config.appName}:${env.BUILD_NUMBER}"
-        }
         stages {
             stage('Lint & Test') {
                 steps { 
                     script {
-                        // 1. tests should exist here
                         sh 'echo "Running linting and tests..."'
                     }
                 }
@@ -18,16 +16,12 @@ def call(Map config = [:]) {
                 steps {
                     withCredentials([string(credentialsId: 'Hug-Face', variable: 'TOKEN')]) {
                         script {
-                            // Create a virtual environment
                             sh 'python3 -m venv venv'
-                            
-                            // Use the pip inside the venv
                             sh './venv/bin/pip install huggingface_hub'
                             
                             config.modelFiles.each { file ->
                                 echo "Downloading ${file.name}..."
                                 sh "mkdir -p ${file.targetDir}"
-                                // Use the python inside the venv
                                 sh """
                                 ./venv/bin/python3 -c "from huggingface_hub import hf_hub_download; \
                                 hf_hub_download(repo_id='${config.hfRepo}', \
@@ -45,12 +39,11 @@ def call(Map config = [:]) {
                 when { branch 'main' }
                 steps {
                     script {
-                        // The model files now exist in the workspace, so Docker COPY will find them
-                        sh "docker build -f ju.Dockerfile -t ${DOCKER_IMAGE} ."
+                        sh "docker build -f ju.Dockerfile -t ${dockerImage} ."
                         withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
                             sh 'echo $PASS | docker login -u $USER --password-stdin'
-                            sh "docker push ${DOCKER_IMAGE}"
-                            sh "docker rmi ${DOCKER_IMAGE}"
+                            sh "docker push ${dockerImage}"
+                            sh "docker rmi ${dockerImage}"
                         }
                     }
                 }
